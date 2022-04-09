239ad4ef442e83fd567b51751a48cfc73182db44
Andy Ortiz <a.ortizg@uniandes.edu.co>
2022-04-09 14:28:37 -0500

    obtains the data to begin processing.

--- app.py
+++ app.py
@@ -13,3 +13,19 @@ file to create a dictionary of user ids and their groups their respective sessio
 their activities if the time between activities exceeds 5 minutes.
 """
 
+import requests
+
+# the url from where the API is to be consumed
+url = "https://api.slangapp.com/challenges/v1/activities"
+
+# the authentication header
+headers = {'Authorization':  'Basic '
+                             'NjU6ckRjd2VyL1BicXN1OGdEMUtGMFFja2JrWWJ3TFNjN2tRbUZ4bXJoQndsUT0='}
+
+# obtain the user activites response from the API
+user_activities = requests.get(url, headers=headers).json()
+
+# the python array containing all activites
+activity_arr = user_activities['activities']
+
+print()
